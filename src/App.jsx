ee67f61427724d4953f1ca8074ca838626c460b1
Vatsal <118737969+balasravatsal@users.noreply.github.com>
2023-05-22 08:22:15 +0000

    multiple returns

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx"
+import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx";
 
 function App() {
-    return (
-        <div className='container'>
-            <Starter />
-        </div>
-    );
+  return (
+    <div className="container">
+      <Starter />
+    </div>
+  );
 }
 
-
 export default App;
-
-
-
-
-
--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,31 +1,51 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
-const url = 'https://api.github.com/users/QuincyLarson';
+const url = "https://api.github.com/users/QuincyLarsons";
 
 const MultipleReturnsFetchData = () => {
-    const [isLoading, setIsLoading] = useState(false)
-    const [isError, setIsError] = useState(false)
-    const [user, setUser] = useState(null)
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const response = await fetch(url)
-                const user = await response.json()
-                console.log(user)
-            } catch (e) {
-                console.log(e)
-            }
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
         }
+        const user = await response.json();
+        setUser(user);
+      } catch (e) {
+        // console.log(e);
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchUser();
+  }, []);
 
-        fetchUser()
-    }, [])
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
+  if (isError) {
+    return <h2>Oops, there was an error</h2>;
+  }
 
-
-    return (
-        <h2></h2>
-
-
-    );
+  return (
+    <div>
+      <img
+        style={{ width: "150px", borderRedius: "25px" }}
+        src={user.avatar_url}
+        alt={user.name}
+      />
+      <h2>{user.name}</h2>
+      <h4>Works at {user.company}</h4>
+      <p>{user.bio}</p>
+    </div>
+  );
 };
+
 export default MultipleReturnsFetchData;
