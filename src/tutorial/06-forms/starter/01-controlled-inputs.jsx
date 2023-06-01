e7604a6a4fed178fa49846762845747de7f3e816
Vatsal <118737969+balasravatsal@users.noreply.github.com>
2023-06-01 10:30:57 +0000

    form-handling

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,46 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <></>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  return (
+    <>
+      <form className="form">
+        <h4>Controlled Input</h4>
+        <div className="form-row">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input 
+            type="email" 
+            className="form-input" 
+            id="email" 
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
 export default ControlledInputs;
