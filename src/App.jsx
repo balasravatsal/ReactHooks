b3f707f2f78b061b13af05af42787b699a82541c
Vatsal <118737969+balasravatsal@users.noreply.github.com>
2023-06-02 05:00:38 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Starter from "./tutorial/06-forms/starter/04-other-inputs"
 // import Final from "./tutorial/06-forms/final/04-other-inputs";
 
 function App() {
-ssssssssssssssssssssssssssssssssssssssssssssss  return (
+  return (
     <div className="container">
       <Starter />
     </div>
